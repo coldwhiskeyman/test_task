83b817dcc3740cde6f8b5e034c7a8bf75f6ed1b7
coldwhiskeyman <eldiablo5783@gmail.com>
2022-05-12 12:43:07 +0300

    init

---
+++ main.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+
+def rate_symbols(filename):
+    with open(filename, encoding='utf8') as file:
+        text = file.read()
+
+    symbol_counter = defaultdict(int)
+    total_symbols = 0
+    for char in text:
+        if char != ' ':
+            symbol_counter[char.lower()] += 1
+            total_symbols += 1
+
+    result = {}
+    for char, number in symbol_counter.items():
+        result[char] = round((number / total_symbols) * 100, 2)
+
+    for char, rating in sorted(result.items(), key=lambda x: x[0]):
+        print(f'{char} - {rating}%')
+
+
+if __name__ == '__main__':
+    rate_symbols('sherlock request create chat.txt')
